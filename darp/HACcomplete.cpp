6994d130484f78b4f73a702fc239bab221ab1bce
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-05-13 15:27:20 +0300

    constructor and bugfix

--- darp/HACcomplete.cpp
+++ darp/HACcomplete.cpp
@@ -1,9 +1,12 @@
 #include "HACcomplete.h"
 #include <math.h>
 using namespace std;
+HACcomplete::HACcomplete(const Graph& g_, const std::vector<task> tasks_):HAC(g_,tasks_)
+{
+}
 float HACcomplete::calculate_distance(const std::vector<task>& cluster1, const std::vector<task> cluster2)
 {
-	float max_cost = INFINITY;
+	float max_cost = -1;
 	for (const task& t : cluster1) {
 		for (const task& t2 : cluster2) {
 			max_cost = max(max_cost, min(g.getEdgeCost(t.deliverNode, t2.pickupNode), g.getEdgeCost(t2.deliverNode, t.pickupNode)));
--- darp/HACcomplete.h
+++ darp/HACcomplete.h
@@ -2,6 +2,8 @@
 #include "HAC.h"
 class HACcomplete: public HAC
 {
+public:
+	HACcomplete(const Graph& g_, const std::vector<task> tasks_);
 	// Inherited via HAC
 	virtual float calculate_distance(const std::vector<task>& cluster1, const std::vector<task> cluster2) override;
 };
