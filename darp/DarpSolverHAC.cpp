3cb9cf1c1113f0bb09b1a7151175b820c758266e
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-05-18 18:10:50 +0300

    generic hac class

--- darp/DarpSolverHAC.cpp
+++ darp/DarpSolverHAC.cpp
@@ -6,12 +6,42 @@ DarpSolverHAC::DarpSolverHAC(const std::vector<task>& tasks_, const std::vector<
 {
 }
 
+void DarpSolverHAC::set_hac_properties(const string& clustering, const string& distance)
+{
+	if (clustering == "single") {
+		hac.calculate_cluster_distance = &HAC::ccd_single;
+	}
+	else if (clustering == "complete") {
+		hac.calculate_cluster_distance = &HAC::ccd_complete;
+	}
+	else if (clustering == "average") {
+		hac.calculate_cluster_distance = &HAC::ccd_average;
+	}
+	else {
+		cerr << " clustering" << clustering << " did not recognized " << endl;
+		exit(-1);
+	}
+	if (distance == "min_sg") {
+		hac.calculate_entity_distance = &HAC::ced_min_sg;
+	}
+	else if (distance == "max_sg") {
+		hac.calculate_entity_distance = &HAC::ced_max_sg;
+	}
+	else if (distance == "middle") {
+		hac.calculate_entity_distance = &HAC::ced_middle;
+	}
+	else {
+		cerr << "distance calculation " << distance << " couldnt recognized" << endl;
+		exit(-1);
+	}
+}
+
 void DarpSolverHAC::makeAssignments()
 {
 	hac.calculate_clusters();
 	vector<vector<task>> bestSplit = hac.get_cluster(agentNodeIds.size());
 	for (int i = 0; i < agentNodeIds.size(); i++) {
-		Node n(agentNodeIds[i], vector<edge>());
+		Node n = graph.getNode(agentNodeIds[i]);
 		BruteSDARP::Ptr p(new BruteSDARP(graph, bestSplit[i], n));
 		planners.push_back(p);
 	}
--- darp/DarpSolverHAC.h
+++ darp/DarpSolverHAC.h
@@ -1,15 +1,14 @@
 #pragma once
 #include "DarpSolverBF.h"
-#include "HACsingle.h"
-#include "HACcomplete.h"
+#include "HAC.h"
 class DarpSolverHAC: public DarpSolverBF
 {
 	// Inherited via DarpSolver
 public:
 	DarpSolverHAC(const std::vector<task>& tasks, const std::vector<int> agentNodeIds, const Graph& g);
-
+	void set_hac_properties(const string& clustering, const string& distance);
 	typedef std::tr1::shared_ptr<DarpSolverHAC> Ptr;
 	virtual void makeAssignments() override;
 private:
-	HACcomplete hac;
+	HAC hac;
 };
