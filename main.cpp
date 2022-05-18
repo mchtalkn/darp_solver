c5b7897bba205df4d8a16c5c49cefadc56da90d3
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-05-18 18:15:57 +0300

    main for testing hac

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <Helpers.h>
 using namespace std;
 int main() {
+	string clustering[3] = {"single","complete","average"};
+	string distances[3] = { "min_sg","max_sg","middle" };
 	string graph_path = "graph.inp";
 	
 	ifstream f;
@@ -13,12 +15,32 @@ int main() {
 	Graph g(f);
 	vector<task> tasks = readTasksFromFile(f);
 	vector<int> agentNodeIds = readAgentNodesFromFile(f);
-
-	DarpSolverBF dsbf(tasks,agentNodeIds,g);
-	DarpSolverHAC dshc(tasks, agentNodeIds, g);
+	DarpSolverBF dsbf(tasks, agentNodeIds, g);
 	dsbf.solve();
-	dshc.solve();
 	auto r1 = dsbf.getRoutes();
-	auto r2 = dshc.getRoutes();
+	float total_cost = 0;
+	float max_cost = 0;
+	for (auto& r : r1) {
+		total_cost += r.cost;
+		max_cost = max(max_cost, r.cost);
+	}
+	printf(" brute force total cost %.0f max cost %.0f \n", total_cost, max_cost);
+	for (const string& c : clustering) {
+		for (const string& d : distances) {
+			total_cost = 0;
+			max_cost = 0;
+			DarpSolverHAC dshc(tasks, agentNodeIds, g);
+			dshc.set_hac_properties(c,d);
+			dshc.solve();
+			r1 = dshc.getRoutes();
+			for (auto& r : r1) {
+				total_cost += r.cost;
+				max_cost = max(max_cost, r.cost);
+			}
+			printf(" HAC %s %s total cost %.0f max cost %.0f \n", c.c_str(), d.c_str(), total_cost, max_cost);
+
+
+		}
+	}
 	int bp = 0;
 }
