c6d77975b119c355e064f4cfad8a559f95801c4a
mchtalkn <mucahit.a.yorganci@gmail.com>
2022-06-03 18:11:45 +0300

    time measurment

--- darp/DarpSolver.h
+++ darp/DarpSolver.h
@@ -6,12 +6,14 @@
 class DarpSolver
 {
 public:
-	typedef std::tr1::shared_ptr<DarpSolver> Ptr;
+	typedef std::shared_ptr<DarpSolver> Ptr;
 	DarpSolver(const std::vector<task>&tasks, const std::vector<int> agentNodeIds);
 	virtual void solve() = 0;
 	virtual void makeAssignments() = 0;
 	virtual std::vector<route> getRoutes();
 	virtual Ptr copy() = 0;
+	long long assignment_time = -1;
+	long long solution_time = -1;
 protected:
 	DarpSolver(const DarpSolver& ds);
 	std::vector<RoutePlanner::Ptr> planners;
